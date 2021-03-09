0b245920867fe5e6b148faabd3b81cb36dc75ba1
0yah <highmeme10@gmail.com>
2021-03-09 13:15:01 +0300

    Added Comprehensive comments

--- main_adaline.py
+++ main_adaline.py
@@ -13,7 +13,7 @@ y = bc.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-adaline = Adaline(number_of_iterations=10)
+adaline = Adaline(number_of_iterations=1000)
 
 
 
