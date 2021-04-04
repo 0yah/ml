060837926987ee3d778b10c29ba858691eda1632
0yah <highmeme10@gmail.com>
2021-04-04 08:15:02 +0300

    Kmean Initialized

---
+++ kmeans.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.datasets._samples_generator import make_blobs
+from sklearn.cluster import KMeans
+
+X,y =make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
